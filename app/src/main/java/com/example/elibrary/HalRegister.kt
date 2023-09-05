7ef97b9d3d95afcdfa9a90face5a2828a13faa26
rayhaaa <rayhanayasmin.2021@student.uny.ac.id>
2023-09-05 22:30:19 +0700

    commit awal

---
+++ app/src/main/java/com/example/elibrary/HalRegister.kt
@@ -0,0 +1,11 @@
+package com.example.elibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HalRegister : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hal_register)
+    }
+}
